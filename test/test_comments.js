ef8795c7afa7eb88d46d55f30c0facc7f8c631f3
David Neon <sdneon@users.noreply.github.com>
2015-07-25 19:37:26 +0800

    Test comments in XML -> JSON conversion.

--- test/test_comments.js
+++ test/test_comments.js
@@ -16,16 +16,28 @@ fs.readFile('simple.scxml', function(err, data) {
                 console.log(err);
                 return;
             }
-            console.log('To retain comments:');
+            console.log('XML -> JSON: To retain comments:');
             console.log(util.inspect(result, false, 5, true));
             if (!result || !result.scxml
                 || !result.scxml['%'])
             {
-                console.log('ERR: Comments not found!\n');
+                console.log('ERR: Comments not found in JSON!\n');
             }
             else
             {
-                console.log('OK: Comments found.\n');
+                console.log('OK: Comments found in JSON.\n');
+                var builder = new xml2js.Builder({headless:true}),
+                    xml = builder.buildObject(result);
+                console.log('JSON -> XML: To retain comments:');
+                console.log(xml);
+                if (xml.indexOf('<!--') >= 0)
+                {
+                    console.log('OK: Comments found in rebuilt XML.\n');
+                }
+                else
+                {
+                    console.log('ERR: Comments not found in rebuilt XML!\n');
+                }
             }
         });
 
@@ -37,16 +49,28 @@ fs.readFile('simple.scxml', function(err, data) {
                 console.log(err);
                 return;
             }
-            console.log('To discard/ignore comments:');
+            console.log('XML -> JSON: To discard/ignore comments:');
             console.log(util.inspect(result, false, 5, true));
             if (result && !result.scxml
                 && result.scxml['%'])
             {
-                console.log('ERR: Comments not expected to be retained!');
+                console.log('ERR: Comments not expected to be retained in JSON!\n');
             }
             else
             {
-                console.log('OK: Comments discarded/not found.');
+                console.log('OK: Comments discarded/not found in JSON.\n');
+                var builder = new xml2js.Builder({headless:true}),
+                    xml = builder.buildObject(result);
+                console.log('JSON -> XML: No comments appear out of thin air:');
+                console.log(xml);
+                if (xml.indexOf('<!--') < 0)
+                {
+                    console.log('OK: Rebuilt XML is empty of comments.\n');
+                }
+                else
+                {
+                    console.log('ERR: Comments appeared in rebuilt XML!\n');
+                }
             }
         });
 });
